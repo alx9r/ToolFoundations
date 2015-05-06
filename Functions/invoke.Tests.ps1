650c5c348c2aa2ae49458c793296499d458e1a95
alx9r <eggy.island9@gmail.com>
2015-05-05 17:11:02 -0700

    Ported invoke from private repo.

---
+++ Functions/invoke.Tests.ps1
@@ -0,0 +1,104 @@
+﻿Describe "Invoke-Ternary" {
+    BeforeEach {
+        Remove-Module a9Foundations -ea SilentlyContinue
+        Import-Module a9Foundations
+    }
+
+    It "correctly interprets boolean true" {
+        $true | ?: 'true' 'false' | Should Be 'true'
+    }
+
+    It "correctly interprets boolean true" {
+        $false | ?: 'true' 'false' | Should Be 'false'
+    }
+
+    It "correctly interprets null" {
+        $null | ?: 'true' 'false' | Should Be 'false'
+    }
+
+    It "correctly interprets empty list" {
+        @() | ?: 'true' 'false' | Should Be 'false'
+    }
+
+    It "correctly interprets empty hash table" {
+        @{} | ?: 'true' 'false' | Should Be 'true'
+    }
+
+    It "correctly interprets list whose last value is false" {
+        @($true,$false) | ?: 'true' 'false' | Should Be 'false'
+    }
+
+    It "correctly interprets list whose last value is true" {
+        @($false,$true) | ?: 'true' 'false' | Should Be 'true'
+    }
+
+    Context "delayed execution of scriptblock" {
+
+        It "correctly delays execution of true scriptblock" {
+            $Global:9b57b26a = 1
+            $Global:9b57b26a | ?: {
+                $Global:9b57b26a
+                $Global:9b57b26a = 0
+            } 'false' | Should be 1
+            $Global:9b57b26a | Should be 0
+        }
+
+        It "correctly delays execution of false scriptblock" {
+            $Global:9b57b26a = 0
+            $Global:9b57b26a | ?: 'false' {
+                $Global:9b57b26a
+                $Global:9b57b26a = 1
+            } | Should be 0
+            $Global:9b57b26a | Should be 1
+        }
+
+        AfterEach {
+            Remove-Variable $Global:9b57b26a -ea Ignore
+        }
+    }
+
+    Context "variable expansion" {
+        BeforeEach {
+            $MyString = 'The $animal says $sound.'
+            $animal = 'fox'
+            $sound = 'simper'
+            $FoxSimper = 'The fox says simper.'
+        }
+
+        It "correctly expands variables in true parameter" {
+            $true | ?: "The $animal says $sound." 'false' | Should be $FoxSimper
+        }
+
+        It "correctly expands variables in scriptblock true parameter" {
+            $true | ?: {"The $animal says $sound."} 'false' | Should be $FoxSimper
+        }
+
+        It "correctly expands variables in false parameter" {
+            $false | ?: 'true' "The $animal says $sound." | Should be $FoxSimper
+        }
+
+        It "correctly expands variables in scriptblock false parameter" {
+            $false | ?: 'true' {"The $animal says $sound."} | Should be $FoxSimper
+        }
+    }
+
+    Context 'no alternate' {
+        It 'emits nothing on false when no alternate is provided.' {
+            $false,0,$null | ?: 'true' | Should BeNullOrEmpty
+        }
+    }
+}
+
+
+Describe "Invoke-IfScriptblock" {
+    BeforeEach {
+        Remove-Module a9Foundations -ea SilentlyContinue
+        Import-Module a9Foundations
+    }
+    It "outputs a non-scriptblock object" {
+        Invoke-IfScriptBlock "string" | Should be 'string'
+    }
+    It "evaluates a scriptblock" {
+        Invoke-IfScriptBlock { 2*2 } | Should be 4
+    }
+}
---
+++ Functions/invoke.ps1
@@ -0,0 +1,62 @@
+# This module is derived from Richard Berg's Invoke-IfScriptBlock.ps1 and Invoke-Ternary.ps1 modules:
+# http://tfstoys.codeplex.com/SourceControl/changeset/view/33350#Modules/RichardBerg-Misc
+
+Set-Alias ?: Invoke-Ternary
+
+<#
+.SYNOPSIS
+If Condition gets coerced to True, emit Primary, otherwise Alternate.
+
+.DESCRIPTION
+The only things that should fail the test are:
+(1) a boolean whose value is False
+(2) an empty collection
+(3) a collection whose last value is boolean false
+(4) null
+
+Primary and Alternate can be wrapped in scriptblocks if you need delayed execution.
+
+Obviously there is similarity between Invoke-Coalescing and -Ternary.  It's mainly just syntax. Both functions are designed to look like the corresponding operators in C-style languages by using the pipeline to simulate infix notation.  (functions are usually prefix, and you can't define your own operators in Powershell)  Pipeline-based construction also allows them to be chained together in interesting [but different] ways.
+
+.EXAMPLE
+1 + 1 -eq 2 | ?: "yay" "new math"
+
+.EXAMPLE
+get-process asdfjkl* | ?: {$null.TotallyIllegal()} {"Ok"}
+
+.EXAMPLE
+# a puzzle - don't cheat
+$false, $true | ?: $false $true | ?: $false $true | ?: $false $true | ?: $false $true
+#>
+
+function Invoke-Ternary
+{
+	[CmdletBinding()]
+    param (
+        [parameter(ValueFromPipeline=$True)]
+        [object]$Condition,
+
+        [parameter(Position=0)]
+        [object]$Primary,
+
+        [parameter(Position=1)]
+        [object]$Alternate
+    )
+
+    end
+    {
+        if ($Condition)
+            { Invoke-IfScriptBlock $Primary }
+        else
+            { Invoke-IfScriptBlock $Alternate }
+    }
+}
+function Invoke-IfScriptBlock($object)
+{
+    if ($object -is [scriptblock]) {
+        & $object
+    }
+    else {
+        $object
+    }
+}
