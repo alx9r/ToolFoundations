1c0cbdfd400ab846bb82dbac7064db2c352d9660
alx9r <eggy.island9@gmail.com>
2015-09-22 14:05:41 -0700

    added Test-ValidFileName

--- Functions/path.Tests.ps1
+++ Functions/path.Tests.ps1
@@ -15,3 +15,41 @@ Describe Test-ValidDriveLetter {
         '1' | Test-ValidDriveLetter | Should be $false
     }
 }
+Describe Test-ValidFilename{
+    BeforeEach {
+        Remove-Module ToolFoundations -ea SilentlyContinue
+        Import-Module ToolFoundations
+    }
+
+    It 'returns true for valid filename.' {
+        'a' | Test-ValidFileName | Should be $true
+        'a.b' | Test-ValidFileName | Should be $true
+    }
+    It 'returns false for an invalid characters.' {
+        'b<d' | Test-ValidFileName | Should be $false
+        'b*d' | Test-ValidFileName | Should be $false
+        'inva|id' | Test-ValidFileName | Should be $false
+        'c:' | Test-ValidFileName | Should be $false
+    }
+    It 'returns false for all periods.' {
+        '.' | Test-ValidFileName | Should be $false
+        '..' | Test-ValidFileName | Should be $false
+    }
+    It 'returns false for DOS Names.' {
+        'PRN.' | Test-ValidFileName | Should be $false
+        'PRN' | Test-ValidFileName | Should be $false
+        'AUX.txt' | Test-ValidFileName | Should be $false
+        'AUXtxt' | Test-ValidFileName | Should be $true
+    }
+    It 'returns false for filename that is too long.' {
+        $s = '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789'+
+        '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789'+
+        '0123456789012345678901234567890123456789012345678901234'
+
+        $s.Length | Should be 255
+        $s | Test-ValidFileName | Should be $true
+        $s = $s+'5'
+        $s.Length | Should be 256
+        $s | Test-ValidFileName | Should be $false
+    }
+}
--- Functions/path.ps1
+++ Functions/path.ps1
@@ -1,4 +1,4 @@
-function Test-ValidDriveLetter
+﻿function Test-ValidDriveLetter
 {
     [CmdletBinding()]
     param
@@ -13,5 +13,48 @@ function Test-ValidDriveLetter
     process
     {
         $DriveLetter -match '^[a-zA-Z]$'
+    }
+}
+function Test-ValidFileName
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(mandatory=$true,
+                   position=1,
+                   ValueFromPipeline=$true)]
+        [string]
+        $FileName
+    )
+    process
+    {
+        # https://stackoverflow.com/a/62888/1404637
+
+        # bad characters
+        $escapedBadChars = ( ([char[]]'<>:"/\|?*') | ConvertTo-RegexEscapedString ) -join '|'
+        if ( $FileName -match "($escapedBadChars)" )
+        { 
+            return $false 
+        }
+
+        # all periods
+        if ( $FileName -match '^\.*$' )
+        {
+            return $false
+        }
+
+        # reserved DOS names
+        if ( $FileName -match '^(PRN|AUX|NUL|CON|COM[1-9]|LPT[1-9])(\.?|\..*)$' )
+        {
+            return $false
+        }
+
+        # length
+        if ($FileName.Length -gt 255 ) 
+        { 
+            return $false 
+        }
+
+        return $true
     }
 }
