904605186b5170c0b4e930fec485525619a46db8
alx9r <eggy.island9@gmail.com>
2015-11-13 14:50:14 -0800

    adding 'pipeline' test

--- EnvironmentTests/parameterValidation.Tests.ps1
+++ EnvironmentTests/parameterValidation.Tests.ps1
@@ -1,3 +1,5 @@
+Import-Module ToolFoundations -Force
+
 Describe 'ValidateScript' {
     Context 'fails validation' {
         function f
@@ -14,6 +16,35 @@ Describe 'ValidateScript' {
             try
             {
                 f -x 1
+            }
+            catch [System.Management.Automation.ParameterBindingException]
+            {
+                $threw = $true
+                $_ | Should match "Cannot validate argument on parameter 'x'"
+            }
+            $threw | Should be $true
+        }
+    }
+    Context 'pipeline' {
+        function f
+        {
+            [CmdletBinding()]
+            param
+            (
+                [parameter(ValueFromPipelineByPropertyName = $true)]
+                [ValidateScript({$false})]
+                $x
+            )
+            process{}
+        }
+        It 'does not throw exception.' {
+            {@{x=1} | >> | f} | Should not throw
+        }
+        It "throws when ErrorActionPreference eq 'Stop'" {
+            $ErrorActionPreference = 'Stop'
+            try
+            {
+                @{x=1} | >> | f
             }
             catch [System.Management.Automation.ParameterBindingException]
             {
