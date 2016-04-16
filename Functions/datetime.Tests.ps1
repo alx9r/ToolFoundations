dc1e45e8c00eeaa84eab4fb9623d78bccc63821b
alx9r <eggy.island9@gmail.com>
2016-04-16 16:39:45 -0700

    add datetime

---
+++ Functions/datetime.Tests.ps1
@@ -0,0 +1,65 @@
+﻿Import-Module ToolFoundations -Force
+
+Describe Get-ThisMonthsPatchTuesday {
+    Context 'returns correct day' {
+        $cases = @(
+            # Input         Output
+            @('2015-12-31', '2015-12-08'),
+            @('2016-01-01', '2016-01-12'),
+            @('2016-01-11', '2016-01-12'),
+            @('2016-01-12', '2016-01-12'),
+            @('2016-01-31', '2016-01-12'),
+            @('2016-02-01', '2016-02-09')
+        )
+        foreach ($case in $cases) {
+            $in = $case[0]
+            $out = $case[1]
+            It $in {
+                $r = $in | Get-ThisMonthsPatchTuesday
+                $r -eq $out | Should be $true
+            }
+        }
+    }
+}
+Describe Get-LastPatchTuesday {
+    Context 'returns correct day' {
+        $cases = @(
+            # Input         Output
+            @('2015-12-31', '2015-12-08'),
+            @('2016-01-01', '2015-12-08'),
+            @('2016-01-11', '2015-12-08'),
+            @('2016-01-12', '2016-01-12'),
+            @('2016-01-31', '2016-01-12'),
+            @('2016-02-01', '2016-01-12')
+        )
+        foreach ($case in $cases) {
+            $in = $case[0]
+            $out = $case[1]
+            It $in {
+                $r = $in | Get-LastPatchTuesday
+                $r -eq $out | Should be $true
+            }
+        }
+    }
+}
+Describe Get-NextPatchTuesday {
+    Context 'returns correct day' {
+        $cases = @(
+            # Input         Output
+            @('2015-12-31', '2016-01-12'),
+            @('2016-01-01', '2016-01-12'),
+            @('2016-01-11', '2016-01-12'),
+            @('2016-01-12', '2016-02-09'),
+            @('2016-01-31', '2016-02-09'),
+            @('2016-02-01', '2016-02-09')
+        )
+        foreach ($case in $cases) {
+            $in = $case[0]
+            $out = $case[1]
+            It $in {
+                $r = $in | Get-NextPatchTuesday
+                $r -eq $out | Should be $true
+            }
+        }
+    }
+}
