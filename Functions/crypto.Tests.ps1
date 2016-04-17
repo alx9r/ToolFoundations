c7f8b64543f45a2e468e7d193cf4d9f6d3ff2c66
alx9r <eggy.island9@gmail.com>
2016-04-17 13:05:57 -0700

    create Get-StringHash

---
+++ Functions/crypto.Tests.ps1
@@ -0,0 +1,55 @@
+Import-Module ToolFoundations -Force
+
+Describe Get-StringHash {
+    Context 'produces correct hash' {
+        $cases = @(
+            # Input            Output
+            @($null,           [string]::Empty),
+            @([string]::Empty, [string]::Empty),
+            @('2015-12-31',    '88C70C62B60E24E699C5EF143097BA0AE291028E047AFE02DBBC0C84EB713BF2'),
+            @('3182feb2',      'E6E6E7771EFBAB77C07078DBD2757E5DF77F41D256CFFDC1210868A97C7F417A' )
+        )
+        foreach ($case in $cases) {
+            $in = $case[0]
+            $out = $case[1]
+            It "Input: $in" {
+                $r = $in | Get-StringHash
+                $r | Should be $out
+            }
+        }        
+    }
+    Context 'produces correct hash (base64)' {
+        $cases = @(
+            # Input            Output
+            @($null,           [string]::Empty),
+            @([string]::Empty, [string]::Empty),
+            @('2015-12-31',    'iMcMYrYOJOaZxe8UMJe6CuKRAo4Eev4C27wMhOtxO/I='),
+            @('3182feb2',      '5ubndx77q3fAcHjb0nV+Xfd/QdJWz/3BIQhoqXx/QXo=' )
+        )
+        foreach ($case in $cases) {
+            $in = $case[0]
+            $out = $case[1]
+            It "Input: $in" {
+                $r = $in | Get-StringHash -OutputEncoding base64
+                $r | Should be $out
+            }
+        }        
+    }
+    Context 'produces correct hash (MD5)' {
+        $cases = @(
+            # Input            Output
+            @($null,           [string]::Empty),
+            @([string]::Empty, [string]::Empty),
+            @('2015-12-31',    '3CD65455CC6F334EC9D8199520886136'),
+            @('3182feb2',      '1C62A2CDD714ADEBA8FAF75FCACFA9B8' )
+        )
+        foreach ($case in $cases) {
+            $in = $case[0]
+            $out = $case[1]
+            It "Input: $in" {
+                $r = $in | Get-StringHash -Algorithm (New-Object System.Security.Cryptography.MD5CryptoServiceProvider)
+                $r | Should be $out
+            }
+        }        
+    }
+}
