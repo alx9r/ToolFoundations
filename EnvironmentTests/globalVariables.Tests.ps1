e281162e18d4ed86073c7881751127a67a6ff4dc
alx9r <eggy.island9@gmail.com>
2016-09-05 09:48:08 -0700

    test that global variables are shared

--- EnvironmentTests/globalVariables.Tests.ps1
+++ EnvironmentTests/globalVariables.Tests.ps1
@@ -7,6 +7,7 @@ Describe 'Global variable naming conflict avoidance' {
                 'g'
                 'v'
                 '_testFile'
+                'guid'
             )
         )
         {
@@ -19,5 +20,27 @@ Describe 'Global variable naming conflict avoidance' {
                 $r | Should beNullOrEmpty
             }
         }
+    }
+}
+$guid = [guid]::NewGuid().GUID.Split('-')[0]
+Describe 'Global variable accessibility' {
+    $module = New-Module (
+        [scriptblock]::Create(
+            @"
+            function f-$guid {
+                `$v_$guid
+            }
+"@
+        )
+    )
+    It 'create a global variable' {
+        Set-Variable "v_$guid" 'global variable' -Scope Global
+    }
+    It 'read that variable back' {
+        Get-Variable "v_$guid" -ValueOnly | Should be 'global variable'
+    }
+    It 'read that variable back inside a different module' {
+        $r = & "f-$guid"
+        $r | Should be 'global variable'
     }
 }
