c753cec5c8cc88ad40571ce22729704cc9661a85
alx9r <eggy.island9@gmail.com>
2016-11-25 07:02:44 -1000

    update tests to reflect behavior between PowerShell 5.0 and 5.1

--- EnvironmentTests/ienumerableMultipleRunspacesTests.ps1
+++ EnvironmentTests/ienumerableMultipleRunspacesTests.ps1
@@ -23,28 +23,45 @@ $h = @{}
 
 Describe 'module containing IEnumerable across runspaces' {
     $i = 0
-    foreach ( $values in @(
+    if ( $PSVersionTable.PSVersion -ge '5.1' )
+    {
+        $label = 'PowerShell 5.1 and later'
+        $tests = @(
+            #  type, result
+            @('ienumerableOfT','^no exception$'),
+            @('ienumerableOfT','^no exception$'),
+            @('ienumerable','^no exception$'),
+            @('ienumerable','^no exception$')
+        )
+    }
+    else
+    {
+        $label = 'prior to PowerShell 5.1'
+        $tests = @(
             #  type, result
             @('ienumerableOfT','^no exception$'),
             @('ienumerableOfT',"The term 'FunctionName' is not recognized"),
             @('ienumerable','^no exception$'),
             @('ienumerable',"The term 'FunctionName' is not recognized")
         )
-    )
-    {
-        $type,$result = $values
-        $modulePath = "$PSScriptRoot\$type.psm1"
-        $i ++
-        It "Runspace $i - getting enumerator from $type results in `"$result`"" {
-            $runspace = [runspacefactory]::CreateRunspace()
-            $ps = [powershell]::Create()
-            $ps.Runspace =$runspace
-            $runspace.Open()
-            $ps.AddScript($scriptblock)
-            $ps.AddParameter('ModulePath',$modulePath)
+    }
+    Context $label {
+        foreach ( $values in $tests )
+        {
+            $type,$result = $values
+            $modulePath = "$PSScriptRoot\$type.psm1"
+            $i ++
+            It "Runspace $i - getting enumerator from $type results in `"$result`"" {
+                $runspace = [runspacefactory]::CreateRunspace()
+                $ps = [powershell]::Create()
+                $ps.Runspace =$runspace
+                $runspace.Open()
+                $ps.AddScript($scriptblock)
+                $ps.AddParameter('ModulePath',$modulePath)
 
-            $r = $ps.Invoke()
-            $r | Should match $result
+                $r = $ps.Invoke()
+                $r | Should match $result
+            }
         }
     }
 }
