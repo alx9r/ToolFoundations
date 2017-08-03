0dd80aab4f29a8250fa96cea17bd0f710a34e411
alx9r <eggy.island9@gmail.com>
2017-08-02 17:45:04 -0700

    manually calculate $PSScriptRoot because PowerShell 2 doesn't

--- EnvironmentTests/pesterInModuleScope.Tests.ps1
+++ EnvironmentTests/pesterInModuleScope.Tests.ps1
@@ -1,4 +1,4 @@
-﻿$path = $PSCommandPath | Split-Path -Parent
+﻿$path = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 if ( (Get-Module Pester).Version -lt '4.0.0' )
 {
     . "$path\pester3InModuleScopeTests.ps1"
