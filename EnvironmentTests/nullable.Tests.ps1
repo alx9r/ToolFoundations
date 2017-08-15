f8f1d0b03af78b1705f7c3fc886373c5f83d64f3
alx9r <eggy.island9@gmail.com>
2017-08-15 07:13:56 -0700

    test nullable enums

---
+++ EnvironmentTests/nullable.Tests.ps1
@@ -0,0 +1,34 @@
+Add-Type "public enum Triple_8c166bf8 { one, two, three }"
+
+Describe 'nullable enum' {
+    Context 'plain' {
+        It 'instantiate' {
+            New-Object Triple_8c166bf8
+        }
+        It 'defaults to first value' {
+            $r = New-Object Triple_8c166bf8
+            $r | Should be 'one'
+        }
+        It 'cannot cast null to enum' {
+            { [Triple_8c166bf8]$null } |
+                Should throw 'Cannot convert null to type'
+        }
+    }
+    Context 'nullable' {
+        It 'instantiate' {
+            New-Object System.Nullable[Triple_8c166bf8]
+        }
+        It 'instantiation results in null' {
+            $r = New-Object System.Nullable[Triple_8c166bf8]
+            $r | Should beNullOrEmpty
+        }
+        It 'can cast null to enum' {
+            $r = [System.Nullable[Triple_8c166bf8]]$null
+            $r | Should beNullOrEmpty
+        }
+        It 'can cast string to enum' {
+            $r = [System.Nullable[Triple_8c166bf8]]'three'
+            $r | Should be 'three'
+        }
+    }
+}
