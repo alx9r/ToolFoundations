0ae4001d40f29e4b0fdc9e409d26c6757c2cee38
alx9r <eggy.island9@gmail.com>
2017-10-03 09:25:03 -0700

    add trace

--- EnvironmentTests/newline.Tests.ps1
+++ EnvironmentTests/newline.Tests.ps1
@@ -1,7 +1,19 @@
+&{
+    '[System.Environment]::newline'
+    ([System.Environment]::NewLine).GetEnumerator() | %{[int]$_}
+    'Here-String'
+    @"
+
+
+"@.GetEnumerator() | %{[int]$_}
+} | Write-Host
+
 Describe newlines {
     It 'System.Environment newline characters' {
         $r = [System.Environment]::NewLine.GetEnumerator() | % {[int]$_ }
-        $r | Should be 13,10
+        $r.Count | Should be 2
+        $r[0] | Should be 13
+        $r[1] | Should be 10
     }
     Context 'here string' {
         It 'empty line is empty string' {
